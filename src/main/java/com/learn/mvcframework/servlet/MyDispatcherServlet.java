bfdf2dccbbff80e27df7e5b91789d74a812b14f8
Loserfromlazy <1320064431@qq.com>
2022-03-08 21:23:49 +0800

    更新代码

--- src/main/java/com/learn/mvcframework/servlet/MyDispatcherServlet.java
+++ src/main/java/com/learn/mvcframework/servlet/MyDispatcherServlet.java
@@ -1,10 +1,7 @@
 package com.learn.mvcframework.servlet;
 
-import com.learn.mvcframework.annotations.MyAutowired;
-import com.learn.mvcframework.annotations.MyController;
-import com.learn.mvcframework.annotations.MyQualifier;
-import com.learn.mvcframework.annotations.MyService;
-import com.sun.deploy.util.StringUtils;
+import com.learn.mvcframework.annotations.*;
+import com.learn.mvcframework.pojo.Handler;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -15,7 +12,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * <p>
@@ -30,7 +31,7 @@ public class MyDispatcherServlet extends HttpServlet {
     /**
      * 存储配置文件
      */
-    private final Properties properties = new Properties();
+    private Properties properties = new Properties();
     /**
      * 存储扫描到的类的全限定类名
      */
@@ -38,7 +39,12 @@ public class MyDispatcherServlet extends HttpServlet {
     /**
      * IOC容器
      */
-    private Map<String,Object> iocMap = new HashMap<>();
+    private Map<String, Object> iocMap = new HashMap<>();
+
+    /**
+     * 处理器映射器，存储url和method的映射关系
+     */
+    private List<Handler> handlerMapping = new ArrayList<>();
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -63,8 +69,40 @@ public class MyDispatcherServlet extends HttpServlet {
      * @author Yuhaoran
      */
     private void initHandlerMapping() {
-        if (iocMap.isEmpty()){
+        if (iocMap.isEmpty()) {
             return;
+        }
+        for (Map.Entry<String, Object> entry : iocMap.entrySet()) {
+            Class<?> aClass = entry.getValue().getClass();
+            if (!aClass.isAnnotationPresent(MyController.class)) {
+                continue;
+            }
+            String baseUrl = "";
+            if (aClass.isAnnotationPresent(MyRequestMapping.class)) {
+                MyRequestMapping annotation = aClass.getAnnotation(MyRequestMapping.class);
+                baseUrl = annotation.value();
+            }
+            Method[] methods = aClass.getMethods();
+            for (Method method : methods) {
+                if (!method.isAnnotationPresent(MyRequestMapping.class)) {
+                    continue;
+                }
+                MyRequestMapping annotation = method.getAnnotation(MyRequestMapping.class);
+                String methorUrl = annotation.value();
+                String url = baseUrl + methorUrl;
+                //封装handler
+                Handler handler = new Handler(entry.getValue(),method, Pattern.compile(url));
+                Parameter[] parameters = method.getParameters();
+                for (int i = 0; i < parameters.length; i++) {
+                    if (parameters[i].getType() == HttpServletRequest.class || parameters[i].getType() ==HttpServletResponse.class){
+                        handler.getParamIndexMapping().put(parameters[i].getType().getSimpleName(),i);
+                    }else {
+                        handler.getParamIndexMapping().put(parameters[i].getName(),i);
+                    }
+                }
+                handlerMapping.add(handler);
+            }
+
         }
 
     }
@@ -75,26 +113,26 @@ public class MyDispatcherServlet extends HttpServlet {
      * @author Yuhaoran
      */
     private void handleAutowired() {
-        if (iocMap.isEmpty()){
+        if (iocMap.isEmpty()) {
             return;
         }
         for (Map.Entry<String, Object> entry : iocMap.entrySet()) {
             Field[] declaredFields = entry.getValue().getClass().getDeclaredFields();
             for (Field field : declaredFields) {
-                if (!field.isAnnotationPresent(MyAutowired.class)){
+                if (!field.isAnnotationPresent(MyAutowired.class)) {
                     continue;
                 }
-                String beanName ="";
-                if (field.isAnnotationPresent(MyQualifier.class)){
+                String beanName = "";
+                if (field.isAnnotationPresent(MyQualifier.class)) {
                     MyQualifier annotation = field.getAnnotation(MyQualifier.class);
                     beanName = annotation.value();
-                }else {
+                } else {
                     //如果没有MyQualifier 那就根据字段类型注入 即接口注入
-                    beanName= field.getType().getName();
+                    beanName = field.getType().getName();
                 }
                 field.setAccessible(true);
                 try {
-                    field.set(entry.getValue(),iocMap.get(beanName));
+                    field.set(entry.getValue(), iocMap.get(beanName));
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
@@ -108,41 +146,41 @@ public class MyDispatcherServlet extends HttpServlet {
      *
      * @author Yuhaoran
      */
-    private void handleInstance(){
+    private void handleInstance() {
         if (classNames.size() == 0) {
             return;
         }
-        try{
+        try {
             for (String className : classNames) {
                 Class<?> aClass = Class.forName(className);
-                if (aClass.isAnnotationPresent(MyController.class)){
+                if (aClass.isAnnotationPresent(MyController.class)) {
                     String simpleName = aClass.getSimpleName();
                     MyController annotation = aClass.getAnnotation(MyController.class);
                     String beanName = annotation.value();
-                    if ("".equals(beanName)||beanName==null){
-                        beanName =lowerFirst(aClass.getSimpleName());
-                        iocMap.put(beanName,aClass.newInstance());
-                    }else {
-                        iocMap.put(beanName,aClass.newInstance());
+                    if ("".equals(beanName) || beanName == null) {
+                        beanName = lowerFirst(aClass.getSimpleName());
+                        iocMap.put(beanName, aClass.newInstance());
+                    } else {
+                        iocMap.put(beanName, aClass.newInstance());
                     }
-                }else if (aClass.isAnnotationPresent(MyService.class)){
+                } else if (aClass.isAnnotationPresent(MyService.class)) {
                     MyService annotation = aClass.getAnnotation(MyService.class);
                     String beanName = annotation.value();
-                    if ("".equals(beanName)||beanName==null){
-                        beanName =lowerFirst(aClass.getSimpleName());
-                        iocMap.put(beanName,aClass.newInstance());
-                    }else {
-                        iocMap.put(beanName,aClass.newInstance());
+                    if ("".equals(beanName) || beanName == null) {
+                        beanName = lowerFirst(aClass.getSimpleName());
+                        iocMap.put(beanName, aClass.newInstance());
+                    } else {
+                        iocMap.put(beanName, aClass.newInstance());
                     }
                     //service层一般都有接口，我们将接口再保存一份，便于自动注入时使用
                     Class<?>[] interfaces = aClass.getInterfaces();
                     for (Class<?> anInterface : interfaces) {
                         //存入接口的全限定类名
-                        iocMap.put(anInterface.getName(),aClass.newInstance());
+                        iocMap.put(anInterface.getName(), aClass.newInstance());
                     }
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -167,7 +205,7 @@ public class MyDispatcherServlet extends HttpServlet {
      * @author Yuhaoran
      */
     private void handleScan(String scanPackage) {
-        String scanPath = Thread.currentThread().getContextClassLoader().getResource("").getPath() + scanPackage.replaceAll("\\.", "\\");
+        String scanPath = Thread.currentThread().getContextClassLoader().getResource("").getPath() + scanPackage.replaceAll("\\.", "\\\\");
         File scanFile = new File(scanPath);
         File[] files = scanFile.listFiles();
         for (File file : files) {
@@ -176,7 +214,7 @@ public class MyDispatcherServlet extends HttpServlet {
                 handleScan(scanPackage + "." + file.getName());
             } else if (file.getName().endsWith(".class")) {
                 //类的全限定类名 com.learn.demo.DemoController
-                String className = scanPackage + file.getName().replaceAll(".class", "");
+                String className = scanPackage + "."+file.getName().replaceAll(".class", "");
                 classNames.add(className);
             }
         }
@@ -198,11 +236,39 @@ public class MyDispatcherServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        doPost(req,resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        //根据url找到对应的方法
+        Handler handler = getHandler(req);
+        if (handler==null){
+            resp.getWriter().write("404 NOT FOUND");
+            return;
+        }
+        // todo 参数绑定
+        Class<?>[] parameterTypes = handler.getMethod().getParameterTypes();
+
+        //handler.getMethod().invoke(handler.getController(),)
+
+    }
+
+    /**
+     * 从handlerMapping获取Handler
+     */
+    private Handler getHandler(HttpServletRequest request){
+        if (handlerMapping.isEmpty()){
+            return null;
+        }
+        String requestURI = request.getRequestURI();
+        for (Handler handler : handlerMapping) {
+            Matcher matcher = handler.getPattern().matcher(requestURI);
+            if (!matcher.matches()){
+                continue;
+            }
+            return handler;
+        }
+        return null;
     }
 }
